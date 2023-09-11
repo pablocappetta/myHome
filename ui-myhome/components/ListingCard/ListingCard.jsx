62ef22738b23ad5d973991689938695ac0fc254d
Pablo Cappetta <75391203+pablocappetta@users.noreply.github.com>
2023-09-11 17:10:18 -0300

    UI improvements

--- ui-myhome/components/ListingCard/ListingCard.jsx
+++ ui-myhome/components/ListingCard/ListingCard.jsx
@@ -3,56 +3,67 @@ import { View, StyleSheet } from "react-native";
 import { Card, Text } from "react-native-paper";
 import ListingTypeChip from "../ListingTypeChip/ListingTypeChip";
 import commaNumber from "comma-number";
+import { useTheme } from "../../contexts/ThemeContext";
 
-const ListingCard = ({ listing, type }) => (
-  <Card
-    style={type === "highlighted" ? styles.highlighted : styles.recent}
-    width={180}
-    height={300}
-  >
-    <View style={styles.listingTypeChip}>
-      <ListingTypeChip listingType={listing.listingType}>
-        {listing.listingType}
-      </ListingTypeChip>
-    </View>
-    <Card.Cover source={{ uri: listing.image }} />
-    <Card.Content style={{ display: "flex", flexDirection: "column" }}>
-      <Text variant="titleSmall" style={{ marginTop: 6 }}>
-        {listing.type}
-      </Text>
-      <Text variant="bodySmall" numberOfLines={2} style={{ width: "100%" }}>
-        {listing.location}
-      </Text>
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "flex-end",
-          alignlistings: "center",
-          marginTop: 12,
-          position: "absolute",
-          top: 56,
-          right: 16,
-        }}
-      >
+const ListingCard = ({ listing, type }) => {
+  const { theme } = useTheme();
+  return (
+    <Card
+      style={type === "highlighted" ? styles.highlighted : styles.recent}
+      width={180}
+      height={300}
+    >
+      <View style={styles.listingTypeChip}>
+        <ListingTypeChip listingType={listing.listingType}>
+          {listing.listingType}
+        </ListingTypeChip>
+      </View>
+      <Card.Cover
+        source={{ uri: listing.image }}
+        defaultSource={
+          theme.dark
+            ? require("../../assets/images/darkBlurredImage.jpg")
+            : require("../../assets/images/blurredImage.jpg")
+        }
+      />
+      <Card.Content style={{ display: "flex", flexDirection: "column" }}>
+        <Text variant="titleSmall" style={{ marginTop: 6 }}>
+          {listing.type}
+        </Text>
+        <Text variant="bodySmall" numberOfLines={2} style={{ width: "100%" }}>
+          {listing.location}
+        </Text>
         <View
           style={{
             display: "flex",
             flexDirection: "row",
-            gap: 4,
+            justifyContent: "flex-end",
+            alignlistings: "center",
+            marginTop: 12,
+            position: "absolute",
+            top: 56,
+            right: 16,
           }}
         >
-          <Text variant="bodyLarge" style={{ fontWeight: 600 }}>
-            {listing.currency}
-          </Text>
-          <Text variant="bodyLarge" style={{ fontWeight: 800 }}>
-            ${commaNumber(listing.price)}
-          </Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 4,
+            }}
+          >
+            <Text variant="bodyLarge" style={{ fontWeight: 600 }}>
+              {listing.currency}
+            </Text>
+            <Text variant="bodyLarge" style={{ fontWeight: 800 }}>
+              ${commaNumber(listing.price)}
+            </Text>
+          </View>
         </View>
-      </View>
-    </Card.Content>
-  </Card>
-);
+      </Card.Content>
+    </Card>
+  );
+};
 
 const sharedStyles = {
   position: "relative",
