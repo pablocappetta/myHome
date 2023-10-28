494644783344a35a394d7ed5f768b3dadb545bc6
Sergio Garroni <serg2404@gmail.com>
2023-10-27 22:29:07 -0300

    Added behavior to favorites

--- ui-myhome/screens/Favorites/Favorites.jsx
+++ ui-myhome/screens/Favorites/Favorites.jsx
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { Text, Dialog, Button } from "react-native-paper";
 import { Appbar } from "react-native-paper";
 import { mockedHighlightedListings } from "../Home/mock/MockedHomeData";
 import ListingFavoriteCard from "./ListingFavoriteCard/ListingFavoriteCard";
@@ -8,7 +9,26 @@ import { useScrollToTop } from "@react-navigation/native";
 const Favorites = ({ navigation }) => {
   const ref = useRef(null);
 
+  const [listingToRemove, setListingToRemove] = useState(null);
+  const [listings, setListings] = useState(mockedHighlightedListings);
+
   useScrollToTop(ref);
+
+  const handleRemoveFromFavoritesAction = () => {
+    //filter listing to remove from listings 
+    const newListings = listings.filter(listing => listing.id !== listingToRemove.id);
+    setListingToRemove(null);
+    setListings(newListings);
+    setDialogVisible(false);
+  }
+
+  const [dialogVisible, setDialogVisible] = useState(false);
+
+  const handleRemoveFavorite = (listing) => {
+    setListingToRemove(listing);
+    setDialogVisible(true);
+  }
+
 
   return (
     <View style={styles.container}>
@@ -18,16 +38,34 @@ const Favorites = ({ navigation }) => {
       </Appbar.Header>
       <ScrollView vertical ref={ref}>
         <View style={styles.containerCardsFavoriteListing}>
-          {mockedHighlightedListings.map((listing) => (
+          {listings.map((listing) => (
             <TouchableOpacity
               key={listing.id}
               onPress={() => navigation.navigate("Post", listing)}
             >
-              <ListingFavoriteCard listing={listing} />
+              <ListingFavoriteCard listing={listing}
+                handleRemoveFavorite={handleRemoveFavorite}
+              />
             </TouchableOpacity>
           ))}
         </View>
       </ScrollView>
+      <Dialog visible={dialogVisible} onDismiss={() => {
+        setDialogVisible(false)
+        setListingToRemove(null)
+      }}>
+        <Dialog.Title>Eliminar favorito</Dialog.Title>
+        <Dialog.Content>
+          <Text>Estas seguro de que queres eliminar esta propiedad de tus favoritos?</Text>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={() => {
+            setListingToRemove(null)
+            setDialogVisible(false)
+          }}>Volver</Button>
+          <Button onPress={handleRemoveFromFavoritesAction}>Si, eliminar</Button>
+        </Dialog.Actions>
+      </Dialog>
     </View>
   );
 };
