2f9a8ff761dc2b88c3bbc2c47293daafa53c18b0
Pablo Cappetta <75391203+pablocappetta@users.noreply.github.com>
2023-11-06 14:30:14 -0300

    Hide clientid

--- api-myhome/src/routes/listing.routes.js
+++ api-myhome/src/routes/listing.routes.js
@@ -7,7 +7,7 @@ const ImgurStorage = require("multer-storage-imgur");
 const multer = require("multer");
 
 const upload = multer({
-  storage: ImgurStorage({ clientId: "95705b589351128" }),
+  storage: ImgurStorage({ clientId: process.env.IMGUR_CLIENT_ID }),
 });
 
 const router = Router();
