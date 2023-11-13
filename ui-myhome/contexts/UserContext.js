24b9dfec74d7f3bd963d35edcc3550cd1473eedb
Pablo Cappetta <75391203+pablocappetta@users.noreply.github.com>
2023-11-12 23:34:17 -0300

    User Context now stores user data locally

--- ui-myhome/contexts/UserContext.js
+++ ui-myhome/contexts/UserContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const UserContext = createContext();
 
@@ -18,6 +19,37 @@ export const UserProvider = ({ children }) => {
 
   const [isUserLogged, setIsUserLogged] = useState(false);
 
+  const setUserDataToAsyncStorage = async (credentials) => {
+    console.debug("Guardando datos de login en AsyncStorage");
+    const stringifiedCredentials = JSON.stringify(credentials);
+    try {
+      await AsyncStorage.setItem("user", stringifiedCredentials);
+      console.log("Datos de login guardados correctamente");
+    } catch (error) {
+      console.error("Error al guardar los datos de login:", error);
+    }
+  };
+
+  const getUserDataFromAsyncStorage = async () => {
+    try {
+      const datosString = await AsyncStorage.getItem("user");
+      if (datosString !== null) {
+        const datosUsuario = JSON.parse(datosString);
+        console.log(
+          "Datos de usuario recuperados correctamente:",
+          datosUsuario
+        );
+        return datosUsuario;
+      } else {
+        console.log("No se encontraron datos de usuario en AsyncStorage");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error al recuperar datos de usuario:", error);
+      return null;
+    }
+  };
+
   const getUserType = () => {
     if (user.isRealtor) return "realtor";
     return "regular";
@@ -27,12 +59,30 @@ export const UserProvider = ({ children }) => {
     setIsUserLogged(user.name !== null);
   }, [user]);
 
+  useEffect(() => {
+    if (isUserLogged && getUserDataFromAsyncStorage() === null) {
+      setUserDataToAsyncStorage(user);
+    }
+  }, [isUserLogged]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const userData = await getUserDataFromAsyncStorage();
+      if (userData && !isUserLogged) {
+        setUser(userData);
+      }
+    };
+    getUserData();
+  }, [AsyncStorage]);
+
   const store = {
     user,
     setUser,
     isUserLogged,
     getUserType,
-    setIsUserLogged
+    setIsUserLogged,
+    getUserDataFromAsyncStorage,
+    setUserDataToAsyncStorage,
   };
 
   return <UserContext.Provider value={store}>{children}</UserContext.Provider>;
