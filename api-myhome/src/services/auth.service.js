4f05f13a5a484d729b9123e73e8ac4d3f2dedb51
mateost <mateostornelli@hotmail.com>
2023-11-27 18:44:49 -0300

    google login y favourites fix (#69)
    * arreglo favourites y agrego login con google
    
    * agrego delete user
    
    * Update api-myhome/src/controllers/user.controller.js
    
    Co-authored-by: Carla Blanco <88563106+carlablanco@users.noreply.github.com>
    
    * Update api-myhome/src/services/users.service.js
    
    Co-authored-by: Carla Blanco <88563106+carlablanco@users.noreply.github.com>
    
    * Update api-myhome/src/services/users.service.js
    
    Co-authored-by: Carla Blanco <88563106+carlablanco@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Carla Blanco <88563106+carlablanco@users.noreply.github.com>

--- api-myhome/src/services/auth.service.js
+++ api-myhome/src/services/auth.service.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
-const RealtorModel = require("../models/Realtor");  
+const RealtorModel = require("../models/Realtor");
+const { InternalServerError } = require("../middlewares/errorHandler");
+const jwt = require("jsonwebtoken");
 
 class AuthService {
   async hasValidCredentials(loginEmail, password) {
@@ -17,6 +19,23 @@ class AuthService {
     } catch (err) {
       console.error(err);
       throw new Error("Error al validar credenciales");
+    }
+  }
+
+  async generateToken(id, type) {
+    try {
+      const token = jwt.sign(
+        {
+          id: id,
+          type: type,
+        },
+        process.env.PRIVATE_KEY,
+        { expiresIn: "1d" }
+      );
+      return token;
+    } catch (err) {
+      console.error(err);
+      throw new InternalServerError("Error al generar token");
     }
   }
 }
